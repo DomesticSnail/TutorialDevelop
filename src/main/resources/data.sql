857df2e3e7af98747875a37eee6b704fcd3b54f1
DomesticSnail <philliptowler94@gmail.com>
2024-12-11 18:50:18 +0900

    1回目のコミット

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO user(name, gender, age, email)
-    VALUES ("キラメキ太郎", "男性", 27, 'taro.kirameki@mail.com');
+     VALUES ("キラメキ太郎", "男性", 27, 'taro.kirameki@mail.com');
 INSERT INTO user(name, gender, age, email)
      VALUES ("キラメキ次郎", "男性", 22, 'jiro.kirameki@mail.com');
 INSERT INTO user(name, gender, age, email)
